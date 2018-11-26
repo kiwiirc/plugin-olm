d78ec26cc5b81ce6e6893424ee193165c711c9b9
Jason Papakostas <vithos@gmail.com>
2018-11-26 13:18:39 -0600

    make encrypted messages styleable. add default styles

--- src/plugin-olm.js
+++ src/plugin-olm.js
@@ -1,11 +1,12 @@
 /* global kiwi */
 
 import olmMiddleware from './middleware'
+import './styles.css'
 
 kiwi.plugin('olm', (client /* , log */) => {
 	// add button to input bar
 	const e2eToggleButton = document.createElement('i')
-	e2eToggleButton.className = 'e2e-toggle-button fa fa-exclamation-triangle'
+	e2eToggleButton.className = 'e2e-toggle-button fa fa-user-secret'
 	client.addUi('input', e2eToggleButton)
 
 	client.on('network.new', newNetworkEvent => {
@@ -19,19 +20,43 @@ kiwi.plugin('olm', (client /* , log */) => {
 			buffer.state.addMessage(buffer, {
 				time: Date.now(),
 				nick: sender,
-				message: `ENCRYPTED: ${text}`,
+				message: text,
 				type: 'privmsg',
+				type_extra: 'encrypted',
+			})
+		})
+
+		ircClient.on('megolm.packet.error', ({ sender, target, error }) => {
+			const buffer = network.bufferByName(target)
+
+			buffer.state.addMessage(buffer, {
+				time: Date.now(),
+				nick: sender,
+				message: String(error),
+				type: 'privmsg',
+				type_extra: 'decryption-error',
 			})
 		})
 	})
 
-	client.on('input.command.e2e', event => {
+	client.on('input.command.msg', event => {
+		const plaintext = event.raw
+		if (plaintext.startsWith('/')) return // don't intercept /msg as command
+
 		event.handled = true
-		const plaintext = event.params
+
 		const buffer = client.state.getActiveBuffer()
 		const target = buffer.name
 		const net = client.state.getActiveNetwork()
 
 		net.ircClient.olm.sendMessage(target, plaintext)
+
+		buffer.state.addMessage(buffer, {
+			time: Date.now(),
+			nick: net.nick,
+			message: plaintext,
+			type: 'privmsg',
+			type_extra: 'encrypted',
+		})
 	})
 })
